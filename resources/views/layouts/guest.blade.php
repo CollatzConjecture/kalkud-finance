87006054751ce733348c56180c8469379a8d1518
Jerry Lucas <jerry.lucas@Jerry-Lucas.local>
2024-05-21 15:48:37 +0700

    feat: add background login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,24 +6,28 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/particle.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+        <div class="relative min-h-screen bg-gray-100 overflow-hidden">
+            <div id="particles-js" class="absolute top-0 left-0 w-full h-full z-0"></div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+            <div class="relative z-10 flex items-center justify-center min-h-screen pointer-events-none">
+                <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg pointer-events-auto">
+                    <div class="flex justify-center">
+                        <a href="/">
+                            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                        </a>
+                    </div>
+                    {{ $slot }}
+                </div>
             </div>
         </div>
     </body>
