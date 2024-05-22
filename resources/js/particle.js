fd95c06863c4fcabff9119c4c0066dba9a810809
Jerry Lucas <jerry.lucas@Jerry-Lucas.local>
2024-05-22 13:56:04 +0700

    fix: modified particle.js

--- resources/js/particle.js
+++ resources/js/particle.js
@@ -48,7 +48,7 @@ particlesJS('particles-js', {
       "line_linked": {
           "enable": true,
           "distance": 150,
-          "color": "#808080",
+          "color": "#1E3A8A",
           "opacity": 0.4,
           "width": 1
       },
