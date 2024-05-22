d4ca3ec26fd63f6c9dae8c18a4f0301c2e53df11
Jerry Lucas <jerry.lucas@Jerry-Lucas.local>
2024-05-22 13:48:33 +0700

    fix: fix css to tailwind

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout title="Beranda">
     <div class="flex flex-col items-center gap-y-10">
-        <h1 class="title-large">Beranda</h1>
+        <h1 class="title-large text-center">Beranda</h1>
         <div class="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
             <div class="chart-container">
                 <canvas id="chart1"></canvas>
